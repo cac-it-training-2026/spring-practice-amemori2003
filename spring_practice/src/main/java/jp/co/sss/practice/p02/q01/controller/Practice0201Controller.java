f53cc4fd6105291d4b0abc8fc8dfd2782d14e952
kureaa <kureaa@C010422904.zipangu.cac.co.jp>
2026-05-28 14:17:48 +0900

    Practice02完了

--- spring_practice/src/main/java/jp/co/sss/practice/p02/q01/controller/Practice0201Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p02/q01/controller/Practice0201Controller.java
@@ -1,5 +1,24 @@
 package jp.co.sss.practice.p02.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
 public class Practice0201Controller {
+	@RequestMapping(path = "/first")
+	public String index2() {
+		return "practice02/01/before_redirect";
+	}
+
+	@RequestMapping(path = "/second")
+	public String index3() {
+		return "redirect:/third";
+	}
+
+	@RequestMapping(path = "/third")
+	public String index4() {
+		return "practice02/01/after_redirect";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p02/q02/controller/Practice0202Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p02/q02/controller/Practice0202Controller.java
@@ -1,5 +1,29 @@
 package jp.co.sss.practice.p02.q02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Practice0202Controller {
+
+	@RequestMapping(path = "/show/redirect_page")
+	public String foward() {
+		return "practice02/02/redirect_link";
+	}
+
+	@RequestMapping(path = "absolute")
+	public String foward2() {
+		return "redirect:https://www.google.co.jp";
+	}
+
+	@RequestMapping(path = "relative")
+	public String foward3() {
+		return "redirect:/result";
+	}
+
+	@RequestMapping(path = "result")
+	public String foward4() {
+		return "practice02/02/redirect_result";
+	}
 
 }
