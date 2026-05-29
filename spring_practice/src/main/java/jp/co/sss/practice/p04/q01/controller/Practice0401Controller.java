b4f2b51399d8765c0a4a65594f03f90efcd5843f
kureaa <kureaa@C010422904.zipangu.cac.co.jp>
2026-05-29 15:49:46 +0900

    Practice04完了 Practice03-1修正

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
@@ -1,5 +1,30 @@
 package jp.co.sss.practice.p04.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p04.q01.form.BmiForm;
+
+@Controller
 public class Practice0401Controller {
+
+	@RequestMapping(path = "/bmi/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice04/01/bmi_input";
+	}
+
+	@RequestMapping(path = "/bmi/result", method = RequestMethod.POST)
+	public String input2(BmiForm bmiForm, Model model) {
+		Double weight = bmiForm.getWeight();
+		Double height = bmiForm.getHeight() / 100;
+		Double bmi = weight / ((height) * (height));
+		model.addAttribute("name", bmiForm.getName());
+		model.addAttribute("weight", bmiForm.getWeight());
+		model.addAttribute("height", bmiForm.getHeight());
+		model.addAttribute("bmi", bmi);
+		return "practice04/01/bmi_result";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
@@ -1,5 +1,46 @@
 package jp.co.sss.practice.p04.q02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class Practice0402Controller {
+
+	@RequestMapping(path = "/numguess/start", method = RequestMethod.GET)
+	public String num(HttpSession session) {
+		Integer hitNumber = (int) (Math.floor(Math.random() * 9) + 1);
+		session.setAttribute("hitNumber", hitNumber);
+		System.out.println("当たり" + hitNumber);
+		return "practice04/02/numguess_start";
+	}
+
+	@RequestMapping(path = "/numguess/input", method = RequestMethod.GET)
+	public String num2() {
+		return "practice04/02/numguess_input";
+	}
+
+	@RequestMapping(path = "/numguess/judge", method = RequestMethod.GET)
+	public String num3(HttpSession session, Integer inputNum) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		if (inputNum == hitNumber) {
+			return "redirect:/numguess/hit";
+		}
+		return "practice04/02/numguess_judge";
+
+	}
+
+	@RequestMapping(path = "/numguess/hit", method = RequestMethod.GET)
+	public String num3(Model model, HttpSession session) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		String message = "当たり！正解は" + hitNumber + "です。";
+
+		session.removeAttribute("hitNumber");
+		model.addAttribute("message", message);
+		return "practice04/02/numguess_end";
+	}
 
 }
